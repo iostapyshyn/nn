97107b16cf65fc64e940cb38ceca86be8e1dbfaf
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2021-05-06 00:02:03 +0200

    declare activations explicitly as extern

--- src/activations.h
+++ src/activations.h
@@ -1,14 +1,14 @@
 /**
  * Copyright (c) 2019 Illia Ostapyshyn
  *
- * Permission is hereby granted, free of charge, to any person obtaining 
+ * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included 
+ *
+ * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
  */
 
@@ -25,7 +25,7 @@ enum {
     ACTIVATIONS_N /* used as the array size for declaration */
 };
 
-double (*activations[ACTIVATIONS_N])(double);
-double (*activations_primes[ACTIVATIONS_N])(double);
+extern double (*activations[ACTIVATIONS_N])(double);
+extern double (*activations_primes[ACTIVATIONS_N])(double);
 
 #endif
